09ab099faf60307dcb68c39293b1d15a6b3c42ba
linux_china <linux_china@hotmail.com>
2017-04-07 09:42:17 +0800

    exposed demo application for test

---
+++ src/test/java/org/mvnsearch/spring/boot/exposed/demo/ExposedSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package org.mvnsearch.spring.boot.exposed.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExposedSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExposedSpringBootStarterApplication.class, args);
+	}
+    
+}
